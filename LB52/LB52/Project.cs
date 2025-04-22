0bb9da4d582b2673252fc57e9e4544deef4da985
maksim05073 <maksimmixcer@gmail.com>
2025-04-22 23:43:00 +0300

    Add files via upload

---
+++ LB52/LB52/Project.cs
@@ -0,0 +1,77 @@
+﻿namespace LB52;
+public class Project
+{
+    private string NameProject;
+    private ProjectType info;
+    private DateTime end;
+    private double price;
+    private Modul[] list;
+    public Project(string NameProject,ProjectType info,DateTime end,double price,Modul[] list)
+    {
+        this.NameProject = NameProject;
+        this.info = info;
+        this.end = end; 
+        this.price = price; 
+        this.list = list ?? new Modul[0];
+    }
+    public override string ToString()
+    {
+        return "Name"+' '+NameProject+" \ninfo"+' '+info+"\nDateEnd"+' '+end+"\nprice"+' '+price+"\nlist"+' '+list;   
+    }
+    public string ToShortString()
+    {
+        double AvgList=0;
+        for (int i = 0; i < list.Length; i++)
+        {
+            AvgList+=list[i].SizeModul;
+        }
+        AvgList /= list.Length;
+        return "Name"+' '+NameProject+" \ninfo"+' '+info+"\nDateEnd"+' '+end+"\nprice"+' '+price+"\nAVGlist"+' '+AvgList;
+    }
+
+    public string nameproject
+    {
+        get => NameProject;
+        set => NameProject = value;
+    }
+    public ProjectType Info
+    {
+        get => info;
+        set => info = value;
+    }public DateTime End
+    {
+        get => end;
+        set => end = value;
+    }public double Price
+    {
+        get => price;
+        set => Price = value;
+    }public Modul[] Name
+    {
+        get => list;
+        set => list = value;
+    }
+
+    public double AVGlist
+    {
+        get
+        {
+            double AvgList=0;
+            for (int i = 0; i < list.Length; i++)
+            {
+                AvgList+=list[i].SizeModul;
+            }
+            AvgList /= list.Length;
+            return AvgList;
+        }
+    }
+
+    public bool Index
+    {
+        get
+        {
+            bool temp = false;
+            for (int i=0;i<)
+        }
+    }
+}
---
+++ LB52/LB52/genPerson.cs
@@ -0,0 +1,19 @@
+﻿namespace LB52;
+
+public class genPerson
+{
+    public static Random rnd=new Random();
+
+    private static string[]
+        ArrayName = { "", "", "", "", "", "", "" };
+
+    private static string[]
+        Surname = { "", "", "", "", "", "" };
+
+    public static DateTime GenDate()
+    {
+        return new DateTime(rnd.Next(1980, 2007), rnd.Next(1, 12), rnd.Next(1, 28));
+    }
+    public static 
+    
+}
